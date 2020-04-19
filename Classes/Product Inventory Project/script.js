dc346690160f57d2f56d355fe611ed072b836e9e
Joann Holland <joann.c.holland@btinternet.com>
2020-04-19 22:18:45 +0100

    Added Inventory class to 'Product Inventory Project'

--- Classes/Product Inventory Project/script.js
+++ Classes/Product Inventory Project/script.js
@@ -39,23 +39,85 @@ class Product {
         this.quantity = quantity
     }
 
-    
+
 }
 
-myproduct = new Product(1, 'Orange', 1.00, 20)
-document.getElementById('productId').innerHTML = myproduct.id
-document.getElementById('productName').innerHTML = myproduct.name
-document.getElementById('productPrice').innerHTML = '£' + myproduct.price.toFixed(2)
-document.getElementById('productQuantity').innerHTML = myproduct.quantity
+// document.getElementById('productId').innerHTML = myproduct.id
+// document.getElementById('productName').innerHTML = myproduct.name
+// document.getElementById('productPrice').innerHTML = '£' + myproduct.price.toFixed(2)
+// document.getElementById('productQuantity').innerHTML = myproduct.quantity
+
+
 
 // create array of products
 let products = [
-    
+    new Product(1, 'Orange', 1.00, 20),
+    new Product(2, 'Apple', 1.00, 20),
+    new Product(3, 'Banana', 1.00, 20),
+    new Product(4, 'Pear', 1.00, 20),
+    new Product(5, 'Plum', 1.00, 20),
+    new Product(6, 'Blueberries', 1.00, 20),
+    new Product(7, 'Mango', 1.00, 20),
+    new Product(8, 'Watermelon', 1.00, 20)
 ]
+
+function addToDOM(products) {
+    let table = document.getElementById('inventoryTable')
+
+    let rowNumber = 1
+    for (let i = 0; i < products.length; i++) {
+        let row = table.insertRow(rowNumber)
+
+        let cell1 = row.insertCell(0)
+        let cell2 = row.insertCell(1)
+        let cell3 = row.insertCell(2)
+        let cell4 = row.insertCell(3)
+
+        cell1.innerHTML = products[i].id
+        cell2.innerHTML = products[i].name
+        cell3.innerHTML = '£' + products[i].price.toFixed(2)
+        cell4.innerHTML = products[i].quantity
+    }
+
+}
+
+addToDOM(products)
 
 // inventory class, keeps track of various products and can sum up the inventory value
 class Inventory {
     constructor() {
+        this.products = []
+    }
+
+    get products() {
+        return this.products
+    }
+
+    set products(products) {
+        for (let i = 0; i < products.length; i++) {
+            this.products.push(products[i])
+        }
 
     }
+
+    getInventoryValue() {
+        let value = 0
+        for (let i = 0; i < products.length; i++) {
+            value += products[i].price
+        }
+        return value
+    }
+
+    removeProduct(productId) {
+        
+    }
+
+}
+
+inventory = new Inventory(products)
+
+document.getElementById('totalInventoryValue').innerHTML = '£' + inventory.getInventoryValue().toFixed(2)
+
+function remove() {
+
 }
