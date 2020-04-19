269f2580ed1a8a18a056f9230168ba7d4a5df4ee
Joann Holland <joann.c.holland@btinternet.com>
2020-04-19 09:59:29 +0100

    Completed 'Find e to the Nth Digit' project

---
+++ Numbers/FInd e to the Nth Digit/script.js
@@ -0,0 +1,11 @@
+function getUserInput() {
+    let userInput = document.getElementById('n-value').value
+    if (userInput <= 0 || userInput > 51) {
+        alert("You need to enter a number between 1 and 51.")
+    } else {
+        let e = Math.E
+        console.log(e.toFixed(51))
+        let result = e.toFixed(userInput)
+        document.getElementById('result').innerHTML = result
+    }
+}
