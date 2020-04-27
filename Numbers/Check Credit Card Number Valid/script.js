781d59f9d15c1d2fd242ed124835a29e41daad90
Joann Holland <joann.c.holland@btinternet.com>
2020-04-27 20:41:18 +0100

    Completed credit card number checker project

--- Numbers/Check Credit Card Number Valid/script.js
+++ Numbers/Check Credit Card Number Valid/script.js
@@ -2,16 +2,37 @@ console.log("connected");
 
 function check() {
   const ccnums = document.getElementById("ccnum").value.split("");
+  const resultSpan = document.getElementById("result");
   const nums = ccnums.map((num) => parseInt(num));
   if (nums.length === 16) {
-    console.log(nums.length);
-    console.log(checksum(nums));
+    let result = checksum(nums);
+    if (result % 10 !== 0) {
+      resultSpan.innerHTML = "This is not a valid credit card number";
+    } else {
+      resultSpan.innerHTML = "This is a valid credit card number";
+    }
   } else {
     alert("Invalid card number - needs to be sixteen digits");
   }
 }
 
 function checksum(nums) {
-  const checknums = nums.slice(0, 15);
-  for (let i = 0; i < 14; i++) {}
+  let checknums = nums.slice(0, 15);
+  for (let i = 2; i <= 14; i += 2) {
+    checknums[i] *= 2;
+  }
+  for (let i = 2; i <= 14; i += 2) {
+    if (checknums[i] >= 10) {
+      checknums[i] =
+        parseInt(checknums[i].toString()[0]) +
+        parseInt(checknums[i].toString()[1]);
+    }
+  }
+  checknums = checknums.reduce((a, b) => a + b, 0);
+  return checknums;
 }
+
+// take first 15 numbers
+// starting with first digit, multiply every second digit by 2
+// every time you have two-digit number, add digits together for one digit result
+// add all numbers together.
